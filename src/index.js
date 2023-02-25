36875f8eee522b95a741c3ab507167f580563a73
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-02-26 00:38:00 +0200

    adds app on hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
+import { ThemeProvider } from '@emotion/react';
 import './index.css';
+
+const theme = {
+  colors: {
+    backgroundColor: '#ffec18',
+    sectionBackgroundColor: '#fcffc7',
+    buttonBackgroundColor: '#ff9b18',
+    goodButtonHoverColor: '#85ff34',
+    neutralButtonHoverColor: '#34ccff',
+    badButtonHoverColor: '#fe6767',
+  },
+  containerBorderRadius: '20px',
+  buttonBorderRadius: '5px',
+  buttonBorder: '1px solid #bb9665',
+  boxShadow: '3px 5px 10px 3px rgba(0, 0, 0, 0.30)',
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
